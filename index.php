eebad1deaa904b7a740a9f48aed718c53e448291
Nikhilroy22 <166237481+Nikhilroy22@users.noreply.github.com>
2025-07-26 13:41:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
-
 <?php
-// This loads the real index.php from root directory
-require __DIR__ . '/public/index.php';
+// Redirect to another page
+header("Location: http://www.njkala.byethost33.com/public/");
+exit; // এটি ব্যবহার করা জরুরি যাতে script এখানেই বন্ধ হয়ে যায়
 ?>
